f3bbf498df9407cbdb1027faa7546e3ba5c982c0
Cauê Farias Santos <cauefsantos@MacBook-Pro-de-Caue.local>
2026-05-15 18:22:38 -0300

    adds `/api/v1/migration` endpoint

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,12 +1,34 @@
 test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+});
+
+test("GET to /api/v1/status should return responseBody with correct format", async () => {
+  const response = await fetch("http://localhost:3000/api/v1/status");
 
   const responseBody = await response.json();
   const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
   expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
+});
 
-  expect(responseBody.dependencies.database.version).toEqual("16.0");
-  expect(responseBody.dependencies.database.max_connections).toEqual(100);
-  expect(responseBody.dependencies.database.opened_connections).toEqual(1);
+test("PostgreSQL version should be 16.0", async () => {
+  const response = await fetch("http://localhost:3000/api/v1/status");
+  const responseBody = await response.json();
+  const databaseVersion = responseBody.dependencies.database.version;
+  expect(databaseVersion).toBe("16.0");
+});
+
+test("PostgreSQL max_connections should be equal to 100", async () => {
+  const response = await fetch("http://localhost:3000/api/v1/status");
+  const responseBody = await response.json();
+  const maxConnections = responseBody.dependencies.database.max_connections;
+  expect(maxConnections).toEqual(100);
+});
+
+test("PostgreSQL opened_connections should be equal to 1", async () => {
+  const response = await fetch("http://localhost:3000/api/v1/status");
+  const responseBody = await response.json();
+  const openedConnections =
+    responseBody.dependencies.database.opened_connections;
+  expect(openedConnections).toEqual(1);
 });
