b6dc9bb6b96b33e3cfb8e478e03b1c84cd7376d1
Cauê Farias Santos <cauefsantos@MacBook-Pro-de-Caue.local>
2026-08-08 11:44:19 -0300

    feat: add support for `PATCH` on `/api/v1/users/[username]`

--- tests/integration/api/v1/users/post.test.js
+++ tests/integration/api/v1/users/post.test.js
@@ -23,9 +23,9 @@ describe("POST /api/v1/users", () => {
           password: "password123",
         }),
       });
-      const responseBody = await response.json();
-
       expect(response.status).toBe(201);
+
+      const responseBody = await response.json();
       expect(responseBody).toEqual({
         id: responseBody.id,
         username: "cauefsantos",
@@ -34,6 +34,9 @@ describe("POST /api/v1/users", () => {
         created_at: responseBody.created_at,
         updated_at: responseBody.updated_at,
       });
+      expect(uuidVersion(responseBody.id)).toBe(4);
+      expect(Date.parse(responseBody.created_at)).not.toBeNaN();
+      expect(Date.parse(responseBody.updated_at)).not.toBeNaN();
 
       const userInDatabase = await user.findOneByUsername("cauefsantos");
       const correctPasswordMatch = await password.compare(
@@ -44,10 +47,6 @@ describe("POST /api/v1/users", () => {
         "wrongPassword",
         userInDatabase.password,
       );
-
-      expect(uuidVersion(responseBody.id)).toBe(4);
-      expect(Date.parse(responseBody.created_at)).not.toBeNaN();
-      expect(Date.parse(responseBody.updated_at)).not.toBeNaN();
       expect(correctPasswordMatch).toBe(true);
       expect(incorrectPasswordMatch).toBe(false);
     });
@@ -82,11 +81,12 @@ describe("POST /api/v1/users", () => {
       expect(response2.status).toBe(400);
       expect(response2Body).toEqual({
         name: "ValidationError",
-        message: "O e-mail informado já está sendo utilizado",
-        action: "Utilize outro e-mail para realizar o cadastro",
+        message: "O e-mail informado já está sendo utilizado.",
+        action: "Utilize outro e-mail para realizar esta operação.",
         status_code: 400,
       });
     });
+
     test("With duplicated 'username'", async () => {
       const response1 = await fetch("http://localhost:3000/api/v1/users", {
         method: "POST",
@@ -117,8 +117,8 @@ describe("POST /api/v1/users", () => {
       expect(response2.status).toBe(400);
       expect(response2Body).toEqual({
         name: "ValidationError",
-        message: "O nome de usuário informado já está sendo utilizado",
-        action: "Utilize outro nome de usuário para realizar o cadastro",
+        message: "O nome de usuário informado já está sendo utilizado.",
+        action: "Utilize outro nome de usuário para realizar esta operação.",
         status_code: 400,
       });
     });
